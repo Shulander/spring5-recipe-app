79da4dffc9cb0c3990f714a844c9aa8b68f12f84
Henrique Vicentini <henrique@vicentini.us>
2019-11-02 17:24:54 -0700

    class 7.107. Create Index page

---
+++ src/main/java/us/vicentini/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package us.vicentini.spring5recipeapp.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        log.info("redirecting to index page");
+        return "index";
+    }
+}
