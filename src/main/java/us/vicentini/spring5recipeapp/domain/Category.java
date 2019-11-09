b66ee392a145e861cd7f864c54edb275355070a4
Henrique Vicentini <henrique@vicentini.us>
2019-11-09 12:22:08 -0800

    class 8.144. Many-To-Many JPA Relationships

---
+++ src/main/java/us/vicentini/spring5recipeapp/domain/Category.java
@@ -0,0 +1,23 @@
+package us.vicentini.spring5recipeapp.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+}
--- src/main/java/us/vicentini/spring5recipeapp/domain/Recipe.java
+++ src/main/java/us/vicentini/spring5recipeapp/domain/Recipe.java
@@ -10,7 +10,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import java.util.Set;
@@ -38,4 +41,9 @@ public class Recipe {
     private Notes notes;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name="category_id"))
+    private Set<Category> categories;
 }
