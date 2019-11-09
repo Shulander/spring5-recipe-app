df66291104513f46ad3d18dcf36db608a0c4b871
Henrique Vicentini <henrique@vicentini.us>
2019-11-09 12:08:43 -0800

    class 8.141. Assignment Create One To One JPA Relationship

--- src/main/java/us/vicentini/spring5recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/us/vicentini/spring5recipeapp/domain/UnitOfMeasure.java
@@ -7,21 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import java.math.BigDecimal;
 
 @Getter
 @Setter
 @Entity
-public class Ingredient {
+public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
-    private BigDecimal amount;
-    @OneToOne
-    private UnitOfMeasure uom;
-    @ManyToOne
-    private Recipe recipe;
 }
