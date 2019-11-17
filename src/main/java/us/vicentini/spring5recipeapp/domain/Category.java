1445be240cdb379eca698634dcf20ad9ef90a5da
Henrique Vicentini <henrique@vicentini.us>
2019-11-17 11:53:30 -0800

    class 10.179. Creating a Junit Test
    this was for domain class, I don't agree we need to test POJO classes

--- src/main/java/us/vicentini/spring5recipeapp/domain/Category.java
+++ src/main/java/us/vicentini/spring5recipeapp/domain/Category.java
@@ -1,5 +1,6 @@
 package us.vicentini.spring5recipeapp.domain;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -13,6 +14,7 @@ import java.util.Set;
 @Data
 @EqualsAndHashCode(exclude = "recipes")
 @Entity
+@Builder
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/test/java/us/vicentini/spring5recipeapp/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package us.vicentini.spring5recipeapp.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CategoryTest {
+    private static final Long CATEGORY_ID = 1L;
+    private static final String CATEGORY_DESCRIPTION = "Description";
+
+    private Category category;
+
+    @BeforeEach
+    void setUp() {
+        category = Category.builder()
+                .id(CATEGORY_ID)
+                .description(CATEGORY_DESCRIPTION)
+                .build();
+    }
+
+    @Test
+    void getId() {
+        assertEquals(CATEGORY_ID, category.getId());
+    }
+
+    @Test
+    void getDescription() {
+        assertEquals(CATEGORY_DESCRIPTION, category.getDescription());
+    }
+}
