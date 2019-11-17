c910c99e6187cb717b29cd9cd9851a5c0117a646
Henrique Vicentini <henrique@vicentini.us>
2019-11-17 13:01:00 -0800

    class 10.184. Introduction to Srpring MockMvc

---
+++ src/test/java/us/vicentini/spring5recipeapp/controllers/IndexControllerMockMvcBuilderTest.java
@@ -0,0 +1,52 @@
+package us.vicentini.spring5recipeapp.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import us.vicentini.spring5recipeapp.recipe.RecipeService;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@ExtendWith(MockitoExtension.class)
+class IndexControllerMockMvcBuilderTest {
+
+    @InjectMocks
+    private IndexController indexController;
+
+    @Mock
+    private RecipeService recipeService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void init() {
+        mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+    }
+
+
+    @Test
+    void getIndexPageMockMvc() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+        verify(recipeService).getRecipes();
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(recipeService);
+    }
+}
---
+++ src/test/java/us/vicentini/spring5recipeapp/controllers/IndexControllerSpringWebMvcTest.java
@@ -0,0 +1,44 @@
+package us.vicentini.spring5recipeapp.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import us.vicentini.spring5recipeapp.recipe.RecipeService;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(IndexController.class)
+class IndexControllerSpringWebMvcTest {
+
+    @MockBean
+    private RecipeService recipeService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getIndexPageMockMvc() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+
+        verify(recipeService).getRecipes();
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(recipeService);
+    }
+}
