b8ddd4be1cc7db145e999e4e466070647dadf943
Henrique Vicentini <henrique@vicentini.us>
2019-11-30 13:02:44 -0800

    class 11.217. Persisting images to Database

--- src/main/java/us/vicentini/spring5recipeapp/services/ImageServiceImpl.java
+++ src/main/java/us/vicentini/spring5recipeapp/services/ImageServiceImpl.java
@@ -1,16 +1,41 @@
 package us.vicentini.spring5recipeapp.services;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import us.vicentini.spring5recipeapp.domain.Recipe;
+import us.vicentini.spring5recipeapp.repositories.RecipeRepository;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
 
     @Override
     public void saveImageFile(Long recipeId, MultipartFile multipartFile) {
+        Recipe recipe = recipeRepository.findById(recipeId)
+                .orElseThrow(() -> {
+                    throw new RuntimeException("Recipe Not Found for id: " + recipeId);
+                });
+        recipe.setImage(getImageBytes(multipartFile));
+        recipeRepository.save(recipe);
+    }
 
-        log.debug("Received a file: " + multipartFile.getOriginalFilename());
+    private Byte[] getImageBytes(MultipartFile multipartFile) {
+        try {
+            byte[] originalFile = multipartFile.getBytes();
+            Byte[] imageBytes = new Byte[originalFile.length];
+            Arrays.setAll(imageBytes, n -> originalFile[n]);
+            return imageBytes;
+        } catch (IOException e) {
+            throw new RuntimeException("Error getting bytes from image: " + multipartFile.getOriginalFilename(), e);
+        }
     }
 }
---
+++ src/test/java/us/vicentini/spring5recipeapp/services/ImageServiceImplTest.java
@@ -0,0 +1,63 @@
+package us.vicentini.spring5recipeapp.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+import us.vicentini.spring5recipeapp.domain.Recipe;
+import us.vicentini.spring5recipeapp.repositories.RecipeRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.WARN)
+class ImageServiceImplTest {
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @InjectMocks
+    private ImageServiceImpl imageService;
+
+    @Test
+    void saveImageFile() throws Exception {
+        //given
+        Long id = 1L;
+        MultipartFile multipartFile = new MockMultipartFile("imagefile", "testing.txt", "text/plain",
+                                                            "Spring Framework Guru".getBytes());
+
+        Recipe recipe = Recipe.builder().id(id).build();
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+
+        //when
+        imageService.saveImageFile(id, multipartFile);
+
+        //then
+        verify(recipeRepository).findById(id);
+        verify(recipeRepository).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+    }
+
+    @AfterEach
+    void tearDown() {
+        verifyNoMoreInteractions(recipeRepository);
+    }
+}
