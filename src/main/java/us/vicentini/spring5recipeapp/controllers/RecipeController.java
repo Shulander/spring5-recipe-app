547326e534e8e704b9e67439b68acc7f7b7759ec
Henrique Vicentini <henrique@vicentini.us>
2019-12-02 22:41:10 -0800

    class 12.235. Showing error data on 404 error page

--- src/main/java/us/vicentini/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/us/vicentini/spring5recipeapp/controllers/RecipeController.java
@@ -69,8 +69,10 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound() {
-        log.error("Handling not found exception");
-        return new ModelAndView("404error");
+    public ModelAndView handleNotFound(NotFoundException ex) {
+        log.error("Handling not found exception: {}", ex.getMessage());
+        ModelAndView modelAndView = new ModelAndView("404error");
+        modelAndView.addObject("exception", ex);
+        return modelAndView;
     }
 }
