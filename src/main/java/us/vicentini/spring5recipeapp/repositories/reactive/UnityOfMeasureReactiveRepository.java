c485a522c5deab98b0cf990c575e3d2177a75f67
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2019-12-24 19:23:30 -0300

    class 21.338. Reactive Repositories

---
+++ src/main/java/us/vicentini/spring5recipeapp/repositories/reactive/UnityOfMeasureReactiveRepository.java
@@ -0,0 +1,10 @@
+package us.vicentini.spring5recipeapp.repositories.reactive;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+import us.vicentini.spring5recipeapp.domain.UnitOfMeasure;
+
+public interface UnityOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+
+    Mono<UnitOfMeasure> findByDescription(String description);
+}
--- src/test/java/us/vicentini/spring5recipeapp/controllers/IndexControllerSpringWebMvcIT.java
+++ src/test/java/us/vicentini/spring5recipeapp/controllers/IndexControllerSpringWebMvcIT.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(IndexController.class)
-class IndexControllerSpringWebMvcTest {
+class IndexControllerSpringWebMvcIT {
 
     @MockBean
     private RecipeService recipeService;
