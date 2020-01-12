2bedf4885be3f3460b8ed27c0130dfdf2034a15e
Henrique Vicentini <henrique@vicentini.us>
2020-01-11 22:53:39 -0800

    class 22.362. WebFlux Exception Handling

--- src/main/java/us/vicentini/spring5recipeapp/controllers/ControllerExceptionHandler.java
+++ src/main/java/us/vicentini/spring5recipeapp/controllers/ControllerExceptionHandler.java
@@ -11,6 +11,7 @@ import us.vicentini.spring5recipeapp.exceptions.NotFoundException;
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NumberFormatException.class)
     public String handleNumberFormatException(NumberFormatException ex, Model model) {
@@ -27,24 +28,4 @@ public class ControllerExceptionHandler {
         model.addAttribute("exception", ex);
         return "404error";
     }
-
-
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormatException(NumberFormatException ex) {
-//        log.error("Handling Number Format Exception: {}", ex.getMessage());
-//        ModelAndView modelAndView = new ModelAndView("400error");
-//        modelAndView.addObject("exception", ex);
-//        return modelAndView;
-//    }
-//
-//
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFoundException(NotFoundException ex) {
-//        log.error("Handling not found exception: {}", ex.getMessage());
-//        ModelAndView modelAndView = new ModelAndView("404error");
-//        modelAndView.addObject("exception", ex);
-//        return modelAndView;
-//    }
 }
